ac09dbd665fe6b8990b32b1c371a974cf4c733dd
Maria Leitão <malufln.99@gmail.com>
2024-10-27 00:10:13 -0300

    feat: add basic report routes

--- src/app/denuncia/denuncia-routing.module.ts
+++ src/app/denuncia/denuncia-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DenunciaDetalheComponent } from './denuncia-detalhe/denuncia-detalhe.component';
+import { DenunciaListagemComponent } from './denuncia-listagem/denuncia-listagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'denuncia/:id', component: DenunciaDetalheComponent },
+  { path: 'denuncia', component: DenunciaListagemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
