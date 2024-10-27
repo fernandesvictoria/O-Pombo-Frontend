d2d95e7e3009aa43887d41fbf3ac656214b7a80d
Maria Leitão <malufln.99@gmail.com>
2024-10-26 23:28:08 -0300

    refactor: add list and read components to pruu module

--- src/app/pruu/pruu.module.ts
+++ src/app/pruu/pruu.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { PruuDetalheComponent } from './pruu-detalhe/pruu-detalhe.component';
+import { PruuListagemComponent } from './pruu-listagem/pruu-listagem.component';
 import { PruuRoutingModule } from './pruu-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [PruuDetalheComponent, PruuListagemComponent],
   imports: [
     CommonModule,
-    PruuRoutingModule
+    PruuRoutingModule,
+    HttpClientModule
   ]
 })
 export class PruuModule { }
