e2867676c1a47a3f1f372f982e3b6275c098856e
Maria Leitão <malufln.99@gmail.com>
2024-10-26 23:20:50 -0300

    feat: add pruu module

---
+++ src/app/pruu/pruu.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PruuRoutingModule } from './pruu-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PruuRoutingModule
+  ]
+})
+export class PruuModule { }
