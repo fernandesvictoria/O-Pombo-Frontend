57245ab8dae922e38c9a2140dae81fadeba607a3
Maria Leitão <malufln.99@gmail.com>
2024-10-26 23:00:22 -0300

    feat: add basic user routes

--- src/app/usuario/usuario-routing.module.ts
+++ src/app/usuario/usuario-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioListagemComponent } from './usuario-listagem/usuario-listagem.component';
+import { UsuarioDetalheComponent } from './usuario-detalhe/usuario-detalhe.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: UsuarioListagemComponent },
+  { path: 'usuario/:id', component: UsuarioDetalheComponent },
+  { path: 'usuario', component: UsuarioListagemComponent }
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
