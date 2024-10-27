74640f0540c40ee57681dcd89b2460caa4be9c78
Maria Leitão <malufln.99@gmail.com>
2024-10-26 23:54:46 -0300

    feat: add report module

---
+++ src/app/denuncia/denuncia.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DenunciaRoutingModule } from './denuncia-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DenunciaRoutingModule
+  ]
+})
+export class DenunciaModule { }
