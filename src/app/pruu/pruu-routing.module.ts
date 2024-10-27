2d4f97262e20dd53d0ec690e4e0ad30c43174183
Maria Leitão <malufln.99@gmail.com>
2024-10-26 23:29:48 -0300

    feat: add basic pruu routes

--- src/app/pruu/pruu-routing.module.ts
+++ src/app/pruu/pruu-routing.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PruuDetalheComponent } from './pruu-detalhe/pruu-detalhe.component';
+import { PruuListagemComponent } from './pruu-listagem/pruu-listagem.component';
+
+const routes: Routes = [
+  { path: '', component: PruuListagemComponent },
+  { path: 'pruu/:id', component: PruuDetalheComponent },
+  { path: 'pruu', component: PruuListagemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
