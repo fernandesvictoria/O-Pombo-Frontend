a9b1dcf755a59f10c2e723bd513211b3169500a1
Maria Leitão <malufln.99@gmail.com>
2024-11-02 18:11:56 -0300

    feat: add report service methods

--- src/app/shared/service/denuncia.service.ts
+++ src/app/shared/service/denuncia.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Denuncia } from '../model/denuncia';
+import { Observable } from 'rxjs';
+import { DenunciaSeletor } from '../seletor/denuncia.seletor';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +11,28 @@ export class DenunciaService {
   private readonly API = 'http://localhost:8080/pombo/denuncia/';
 
   constructor(private httpClient: HttpClient) { }
+
+  salvar(novoDenuncia: Denuncia): Observable<Denuncia> {
+    return this.httpClient.post<Denuncia>(this.API + '/salvar', novoDenuncia)
+  }
+
+  editar(DenunciaEditado: Denuncia): Observable<boolean> {
+    return this.httpClient.put<boolean>(this.API + '/alterar', DenunciaEditado)
+  }
+
+  excluir(DenunciaID: number): Observable<boolean> {
+    return this.httpClient.delete<boolean>(this.API + '/excluir/' + DenunciaID)
+  }
+
+  consultarTodosDenuncias(): Observable<Array<Denuncia>> {
+    return this.httpClient.get<Array<Denuncia>>(this.API + '/todos')
+  }
+
+  listarComSeletor(seletor: DenunciaSeletor): Observable<Array<Denuncia>> {
+    return this.httpClient.post<Array<Denuncia>>(this.API + '/filtrar', seletor);
+  }
+
+  consultarDenunciaId(idDenuncia: number): Observable<Denuncia> {
+    return this.httpClient.get<Denuncia>(this.API + '/consultar/' + idDenuncia)
+  }
 }
