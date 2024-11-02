f6cb3ab9f1bc2fdd40d0de345f70ae37366c74ec
Maria Leitão <malufln.99@gmail.com>
2024-11-02 18:10:18 -0300

    feat: add user service methods

--- src/app/shared/service/usuario.service.ts
+++ src/app/shared/service/usuario.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Usuario } from '../model/usuario';
+import { Observable } from 'rxjs';
+import { UsuarioSeletor } from '../seletor/usuario.seletor';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +10,29 @@ import { Injectable } from '@angular/core';
 export class UsuarioService {
   private readonly API = 'http://localhost:8080/pombo/usuario/';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
+
+  salvar(novoUsuario: Usuario): Observable<Usuario> {
+    return this.httpClient.post<Usuario>(this.API + '/salvar', novoUsuario)
+  }
+
+  editar(UsuarioEditado: Usuario): Observable<boolean> {
+    return this.httpClient.put<boolean>(this.API + '/alterar', UsuarioEditado)
+  }
+
+  excluir(UsuarioID: number): Observable<boolean> {
+    return this.httpClient.delete<boolean>(this.API + '/excluir/' + UsuarioID)
+  }
+
+  consultarTodosUsuarios(): Observable<Array<Usuario>> {
+    return this.httpClient.get<Array<Usuario>>(this.API + '/todos')
+  }
+
+  listarComSeletor(seletor: UsuarioSeletor): Observable<Array<Usuario>> {
+    return this.httpClient.post<Array<Usuario>>(this.API + '/filtrar', seletor);
+  }
+
+  consultarUsuarioId(idUsuario: number): Observable<Usuario> {
+    return this.httpClient.get<Usuario>(this.API + '/consultar/' + idUsuario)
+  }  
 }
