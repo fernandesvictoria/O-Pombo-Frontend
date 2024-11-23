ede7600e3aa4bd094b9c29a72ef31d66e7230bec
Maria Leitão <malufln.99@gmail.com>
2024-11-23 00:58:35 -0300

    feat: pruu service

--- src/app/shared/service/pruu.service.ts
+++ src/app/shared/service/pruu.service.ts
@@ -8,35 +8,43 @@ import { PruuSeletor } from '../seletor/pruu.seletor';
   providedIn: 'root'
 })
 export class PruuService {
-  private readonly API = 'http://localhost:8080/pombo/pruu/';
+  private readonly API = 'http://localhost:8080/pombo/pruu';
 
   constructor(private httpClient: HttpClient) { }
 
-  uploadImagem(pruuId: string, formData: FormData): Observable<any> {
-    return this.httpClient.post(`${this.API}/${pruuId}/upload`, formData);
+  uploadImagem(idPruu: string, formData: FormData): Observable<any> {
+    return this.httpClient.post(`${this.API}/${idPruu}/upload`, formData);
   }
 
   salvar(novoPruu: Pruu): Observable<Pruu> {
-    return this.httpClient.post<Pruu>(this.API + '/salvar', novoPruu)
+    return this.httpClient.post<Pruu>(this.API + '/postar', novoPruu)
   }
 
-  editar(PruuEditado: Pruu): Observable<boolean> {
-    return this.httpClient.put<boolean>(this.API + '/alterar', PruuEditado)
+  // editar(PruuEditado: Pruu): Observable<boolean> {
+  //   return this.httpClient.put<boolean>(this.API + '/editar', PruuEditado)
+  // }
+
+  excluir(idPruu: string): Observable<boolean> {
+    return this.httpClient.delete<boolean>(this.API + '/excluir/' + idPruu)
   }
 
-  excluir(PruuID: number): Observable<boolean> {
-    return this.httpClient.delete<boolean>(this.API + '/excluir/' + PruuID)
+  listarTodos(): Observable<Array<Pruu>> {
+    return this.httpClient.get<Array<Pruu>>(this.API)
   }
-
-  consultarTodosPruus(): Observable<Array<Pruu>> {
-    return this.httpClient.get<Array<Pruu>>(this.API + '/todos')
+  
+  listarPorId(idPruu: number): Observable<Pruu> {
+    return this.httpClient.get<Pruu>(this.API + `/${idPruu}`);
   }
 
   listarComSeletor(seletor: PruuSeletor): Observable<Array<Pruu>> {
     return this.httpClient.post<Array<Pruu>>(this.API + '/filtrar', seletor);
   }
 
-  consultarPruuId(idPruu: number): Observable<Pruu> {
-    return this.httpClient.get<Pruu>(this.API + '/consultar/' + idPruu)
+  listarPruusPorUsuario(idUsuario: string): Observable<Array<Pruu>> {
+    return this.httpClient.get<Array<Pruu>>(this.API + `/usuario/${idUsuario}`);
+  }
+
+  bloquear(idUsuario: string, idPruu: string): Observable<boolean> {
+    return this.httpClient.put<boolean>(this.API + `/${idPruu}/bloquear/${idUsuario}`, {});
   }
 }
