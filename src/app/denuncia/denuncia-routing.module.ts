6e9ed0aeed3678500ade520ccf116937d1d24952
Maria Leitão <malufln.99@gmail.com>
2024-11-26 12:14:59 -0300

    feat: refine report routes

--- src/app/denuncia/denuncia-routing.module.ts
+++ src/app/denuncia/denuncia-routing.module.ts
@@ -4,8 +4,12 @@ import { DenunciaDetalheComponent } from './denuncia-detalhe/denuncia-detalhe.co
 import { DenunciaListagemComponent } from './denuncia-listagem/denuncia-listagem.component';
 
 const routes: Routes = [
-  { path: 'denuncias/:id', component: DenunciaDetalheComponent },
-  { path: 'denuncias', component: DenunciaListagemComponent }
+  {
+    path: '', component: DenunciaListagemComponent
+  },
+  {
+    path: 'cadastro', component: DenunciaDetalheComponent
+  },
 ];
 
 @NgModule({
