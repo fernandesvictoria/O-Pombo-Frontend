3022471aaa818251d417e331bba803558b6ba252
Maria Leitão <malufln.99@gmail.com>
2024-11-26 14:12:09 -0300

    fix: merge

--- src/app/pruu/pruu-cadastro/pruu-cadastro.component.ts
+++ src/app/pruu/pruu-cadastro/pruu-cadastro.component.ts
@@ -8,66 +8,6 @@ import Swal from 'sweetalert2';
   selector: 'app-pruu-cadastro',
   templateUrl: './pruu-cadastro.component.html',
 })
-<<<<<<< HEAD
-export class PruuCadastroComponent {
-  public pruu: Pruu = new Pruu();
-  public selectedFile: File | null = null;
-  public imagePreview: string | ArrayBuffer | null = null;
-
-  constructor(private pruuService: PruuService, private router: Router) {}
-
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    if (file && file.size <= 10 * 1024 * 1024) {
-      this.selectedFile = file;
-
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.imagePreview = reader.result;
-      };
-      reader.readAsDataURL(file);
-    } else {
-      alert('Imagem muito grande! Selecione uma imagem menor que 10MB.');
-      this.selectedFile = null;
-      this.imagePreview = null;
-    }
-  }
-
-  inserir(): void {
-    this.pruuService.cadastrar(this.pruu).subscribe(
-      (resposta) => {
-        Swal.fire('Pruu enviado!', '', 'success');
-        if (this.selectedFile) {
-          // this.uploadImagem(resposta.id);
-        } else {
-          this.voltar();
-        }
-        this.voltar();
-      },
-      (erro) => {
-        Swal.fire('Erro ao enviar seu pruu: ' + erro.error, 'error');
-      }
-    );
-  }
-
-  // uploadImagem(pruuId: string): void {
-  //   const formData = new FormData();
-  //   formData.append('imagem', this.selectedFile!, this.selectedFile!.name);
-
-  //   this.pruuService.uploadImagem(pruuId, formData).subscribe({
-  //     next: () => {
-  //       Swal.fire('Imagem carregada com sucesso!', '', 'success');
-  //       this.voltar();
-  //     },
-  //     error: (erro) => {
-  //       Swal.fire('Erro ao fazer upload da imagem: ' + erro.error, 'error');
-  //     },
-  //   });
-  // }
-
-  voltar(): void {
-    this.router.navigate(['/']); // REVER ROTA
-=======
 export class PruuCadastroComponent implements OnInit {
   pruu: Pruu = new Pruu();
 
@@ -94,6 +34,5 @@ export class PruuCadastroComponent implements OnInit {
 
   public cancelar(): void {
     this.router.navigate(['/pruu']);
->>>>>>> c0b3e78 (feat: create pruu)
   }
 }
