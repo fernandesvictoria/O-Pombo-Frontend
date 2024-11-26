2a71d0e0de801f5e3da4a0d817d65e93074efcf1
Maria Leitão <malufln.99@gmail.com>
2024-11-26 18:23:20 -0300

    fix: pruu logic

--- src/app/denuncia/denuncia-detalhe/denuncia-detalhe.component.ts
+++ src/app/denuncia/denuncia-detalhe/denuncia-detalhe.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
 import { Denuncia } from "../../shared/model/denuncia";
 import { Pruu } from "../../shared/model/pruu";
+import { ActivatedRoute, Router } from "@angular/router";
 import { DenunciaService } from "../../shared/service/denuncia.service";
-import { PruuService } from "../../shared/service/pruu.service";
 
 @Component({
   selector: 'app-denuncia-detalhe',
@@ -11,39 +10,31 @@ import { PruuService } from "../../shared/service/pruu.service";
 })
 export class DenunciaDetalheComponent implements OnInit {
   denuncia!: Denuncia;
-  pruusDenunciados: Pruu[] = [];
   idDenuncia!: number;
+  pruu!: Pruu;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private denunciaService: DenunciaService,
-    private pruuService: PruuService
+    private denunciaService: DenunciaService
   ) { }
 
   ngOnInit(): void {
     this.idDenuncia = Number(this.route.snapshot.paramMap.get('id'));
     this.carregarDenuncia();
-    this.carregarPruusDenunciados();
   }
 
   carregarDenuncia(): void {
     this.denunciaService.pesquisarPorId(this.idDenuncia).subscribe({
-      next: (denuncia) => (this.denuncia = denuncia),
+      next: (denuncia) => {
+        this.denuncia = denuncia;
+        this.pruu = denuncia.pruu; 
+      },
       error: (erro) => {
         console.error('Erro ao carregar denúncia', erro);
         this.voltarParaListagem();
       },
     });
-  }
-
-  carregarPruusDenunciados(): void {
-    this.pruuService
-      .pesquisarComFiltro({ idDenuncia: this.idDenuncia } as any) // Ajuste o seletor conforme necessário
-      .subscribe({
-        next: (pruus) => (this.pruusDenunciados = pruus),
-        error: (erro) => console.error('Erro ao carregar pruus denunciados', erro),
-      });
   }
 
   voltarParaListagem(): void {
