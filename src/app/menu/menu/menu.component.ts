f591dddc4d175b70d4c94e4cfc129af25ea3ac69
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2024-11-26 13:58:00 -0300

    fix merge

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -1,24 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoginService } from '../../shared/service/login.service';
+import { jwtDecode } from 'jwt-decode';
+import { Usuario } from '../../shared/model/usuario';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
 })
 export class MenuComponent implements OnInit {
-  constructor(
-    private service: LoginService,
-    private router: Router,
-  ) { }
 
-  ngOnInit(): void { }
+  public UsuarioAutenticado: Usuario | undefined;
+  public isAdmin: boolean = false;
 
-  public sair() {
-    this.service.sair();
-    this.router.navigate(['/login']);
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    let token = localStorage.getItem('tokenUsuarioAutenticado');
+
+    if (token) {
+      // Converte a String para o formato JSON
+      let tokenJSON: any = jwtDecode(token);
+      this.isAdmin = tokenJSON?.roles == 'ADMINISTRADOR';
+
+      if (this.isAdmin) {
+        this.router.navigate(['/pruus']);
+      }
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
 
+  logout() {
+    localStorage.removeItem('tokenUsuarioAutenticado');
+    this.router.navigate(['/login']);
+  }
 }
 
 
