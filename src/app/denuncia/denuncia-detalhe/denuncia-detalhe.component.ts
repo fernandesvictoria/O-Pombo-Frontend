f25e5129081a9281adcebc71f2494878afc3c214
Maria Leitão <malufln.99@gmail.com>
2024-11-26 18:05:12 -0300

    temporary: disable files for test

--- src/app/denuncia/denuncia-detalhe/denuncia-detalhe.component.ts
+++ src/app/denuncia/denuncia-detalhe/denuncia-detalhe.component.ts
@@ -20,42 +20,42 @@ export class DenunciaDetalheComponent implements OnInit {
   ngOnInit(): void {
     // obter o ID da denúncia a partir da rota
     this.idDenuncia = Number(this.route.snapshot.paramMap.get('id'));
-    this.carregarDenuncia();
+    //this.carregarDenuncia();
   }
 
-  carregarDenuncia(): void {
-    this.denunciaService.pesquisarPorId(this.idDenuncia).subscribe({
-      next: (denuncia) => this.denuncia = denuncia,
-      error: (erro) => {
-        console.error('Erro ao carregar denúncia', erro);
-        this.voltarParaListagem();
-      }
-    });
-  }
+  // carregarDenuncia(): void {
+  //   this.denunciaService.pesquisarPorId(this.idDenuncia).subscribe({
+  //     next: (denuncia) => this.denuncia = denuncia,
+  //     error: (erro) => {
+  //       console.error('Erro ao carregar denúncia', erro);
+  //       this.voltarParaListagem();
+  //     }
+  //   });
+  // }
 
-  voltarParaListagem(): void {
-    this.router.navigate(['/denuncia']);
-  }
+  // voltarParaListagem(): void {
+  //   this.router.navigate(['/denuncia']);
+  // }
 
-  excluirDenuncia(): void {
-    if (confirm('Tem certeza que deseja excluir esta denúncia?')) {
-      this.denunciaService.excluir(this.idDenuncia.toString()).subscribe({
-        next: () => {
-          console.log('Denúncia excluída com sucesso!');
-          this.voltarParaListagem();
-        },
-        error: (erro) => console.error('Erro ao excluir denúncia', erro)
-      });
-    }
-  }
+  // excluirDenuncia(): void {
+  //   if (confirm('Tem certeza que deseja excluir esta denúncia?')) {
+  //     this.denunciaService.excluir(this.idDenuncia.toString()).subscribe({
+  //       next: () => {
+  //         console.log('Denúncia excluída com sucesso!');
+  //         this.voltarParaListagem();
+  //       },
+  //       error: (erro) => console.error('Erro ao excluir denúncia', erro)
+  //     });
+  //   }
+  // }
 
-  atualizarStatus(novoStatus: string): void {
-    this.denunciaService.atualizar(this.idDenuncia.toString(), novoStatus as any).subscribe({
-      next: (denunciaAtualizada) => {
-        console.log('Status atualizado com sucesso:', denunciaAtualizada);
-        this.carregarDenuncia();
-      },
-      error: (erro) => console.error('Erro ao atualizar status', erro)
-    });
-  }
+  // atualizarStatus(novoStatus: string): void {
+  //   this.denunciaService.atualizar(this.idDenuncia.toString(), novoStatus as any).subscribe({
+  //     next: (denunciaAtualizada) => {
+  //       console.log('Status atualizado com sucesso:', denunciaAtualizada);
+  //       this.carregarDenuncia();
+  //     },
+  //     error: (erro) => console.error('Erro ao atualizar status', erro)
+  //   });
+  // }
 }
