c0edca1900b88fd8e948052ea75553d59185dc8a
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2024-11-26 19:41:56 -0300

    feat: update filter on listagem_pruu interface

--- src/app/pruu/pruu-listagem/pruu-listagem.component.ts
+++ src/app/pruu/pruu-listagem/pruu-listagem.component.ts
@@ -8,16 +8,20 @@ import { Denuncia } from '../../shared/model/denuncia';
 import { DenunciaService } from '../../shared/service/denuncia.service';
 import { Motivo } from '../../shared/model/enum/motivo';
 import { StatusDenuncia } from '../../shared/model/enum/status-denuncia';
+import { UsuarioService } from '../../shared/service/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pruu-listagem',
   templateUrl: './pruu-listagem.component.html',
 })
 export class PruuListagemComponent implements OnInit {
+  usuarios: Usuario[] = [];
   pruus: Pruu[] = [];
   filtroAtivo: boolean = false;
   pruuSeletor: PruuSeletor = new PruuSeletor();
   totalPaginas: number = 0;
+  mensagemErro: string = '';
   readonly itensPorPagina: number = 5;
 
   usuarioAutenticado!: Usuario;
@@ -25,30 +29,61 @@ export class PruuListagemComponent implements OnInit {
   constructor(
     private pruuService: PruuService,
     private denunciaService: DenunciaService,
+    private usuarioService: UsuarioService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.pruuSeletor.limite = this.itensPorPagina;
     this.pruuSeletor.pagina = 1;
 
     this.pesquisarTodos();
+    this.buscarUsuarios();
+  }
+
+
+
+  private buscarUsuarios() {
+    this.usuarioService.pesquisarTodos().subscribe(
+      (r) => {
+        this.usuarios = r;
+      },
+      (e) => {
+        Swal.fire({
+          title: 'Erro!',
+          text: 'Erro ao consultar todos os clientes: ' + e.error.mensagem,
+          icon: 'error',
+        });
+      }
+    );
   }
 
   pesquisarTodos(): void {
     this.pruuService.pesquisarTodos(this.pruuSeletor).subscribe({
-      next: pruus => this.pruus = pruus,
-      error: erro => console.error('Erro ao buscar Pruus', erro)
+      next: (pruus) => (this.pruus = pruus),
+      error: (erro) => console.error('Erro ao buscar Pruus', erro),
     });
   }
 
   aplicarFiltros(): void {
+    // Ajusta as datas para garantir que a hora seja 00:00
+    if (this.pruuSeletor.criadoEmInicio) {
+      this.pruuSeletor.criadoEmInicio = this.ajustarDataParaInicioDoDia(
+        this.pruuSeletor.criadoEmInicio
+      );
+    }
+    if (this.pruuSeletor.criadoEmFim) {
+      this.pruuSeletor.criadoEmFim = this.ajustarDataParaInicioDoDia(
+        this.pruuSeletor.criadoEmFim
+      );
+    }
+
     this.pruuService.pesquisarComFiltro(this.pruuSeletor).subscribe({
-      next: pruus => {
+      next: (pruus) => {
         this.pruus = pruus;
         this.filtroAtivo = true;
       },
-      error: erro => console.error('Erro ao aplicar filtros', erro)
+      error: (erro) => console.error('Erro ao aplicar filtros', erro),
     });
   }
 
@@ -59,10 +94,14 @@ export class PruuListagemComponent implements OnInit {
   }
 
   curtir(pruu: Pruu): void {
-    const usuarioJaCurtiu = pruu.usuariosQueCurtiram.some(usuario => usuario.id === this.usuarioAutenticado.id);
+    const usuarioJaCurtiu = pruu.usuariosQueCurtiram.some(
+      (usuario) => usuario.id === this.usuarioAutenticado.id
+    );
 
     if (usuarioJaCurtiu) {
-      pruu.usuariosQueCurtiram = pruu.usuariosQueCurtiram.filter(usuario => usuario.id !== this.usuarioAutenticado.id);
+      pruu.usuariosQueCurtiram = pruu.usuariosQueCurtiram.filter(
+        (usuario) => usuario.id !== this.usuarioAutenticado.id
+      );
       pruu.quantidadeCurtidas--;
     } else {
       pruu.usuariosQueCurtiram.push(this.usuarioAutenticado);
@@ -70,8 +109,9 @@ export class PruuListagemComponent implements OnInit {
     }
 
     this.pruuService.curtir(pruu.id, this.usuarioAutenticado.id).subscribe({
-      next: () => console.log('Ação de curtir/descurtir realizada com sucesso!'),
-      error: erro => console.error('Erro ao curtir/descurtir', erro)
+      next: () =>
+        console.log('Ação de curtir/descurtir realizada com sucesso!'),
+      error: (erro) => console.error('Erro ao curtir/descurtir', erro),
     });
   }
 
@@ -81,12 +121,19 @@ export class PruuListagemComponent implements OnInit {
       pruu: pruu,
       usuario: this.usuarioAutenticado,
       motivo: motivo,
-      status: StatusDenuncia.PENDENTE
+      status: StatusDenuncia.PENDENTE,
     };
 
     this.denunciaService.cadastrar(novaDenuncia).subscribe({
       next: () => console.log('Denúncia criada com sucesso!'),
-      error: erro => console.error('Erro ao criar denúncia', erro)
+      error: (erro) => console.error('Erro ao criar denúncia', erro),
     });
+  }
+
+  // Função para ajustar a data para o início do dia (00:00:00)
+  ajustarDataParaInicioDoDia(data: Date): Date {
+    const date = new Date(data);
+    date.setHours(0, 0, 0, 0); // Define a hora para 00:00:00
+    return date; // Retorna um objeto Date
   }
 }
