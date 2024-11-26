a0ee167cc9eaf20e722ad5a07afeaa51e0fa21cf
Maria Leitão <malufln.99@gmail.com>
2024-11-26 14:05:48 -0300

    fix: menu

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -1,50 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-<<<<<<< HEAD
-import { jwtDecode } from 'jwt-decode';
-import { Usuario } from '../../shared/model/usuario';
-=======
 import { LoginService } from '../../shared/service/login.service';
 import { AuthService } from '../../shared/service/auth.service';
->>>>>>> caad81b (refactor: layout menu)
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
 })
 export class MenuComponent implements OnInit {
+  usuarioAdmin: boolean = false;
 
-<<<<<<< HEAD
-  public UsuarioAutenticado: Usuario | undefined;
-  public isAdmin: boolean = false;
-=======
   constructor(
     private service: LoginService,
     private authService: AuthService,
     private router: Router,
   ) { }
->>>>>>> caad81b (refactor: layout menu)
 
-  constructor(private router: Router) { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
-    let token = localStorage.getItem('tokenUsuarioAutenticado');
-
-    if (token) {
-      // Converte a String para o formato JSON
-      let tokenJSON: any = jwtDecode(token);
-      this.isAdmin = tokenJSON?.roles == 'ADMINISTRADOR';
-
-      if (this.isAdmin) {
-        this.router.navigate(['/pruus']);
-      }
-    } else {
-      this.router.navigate(['/login']);
-    }
+  public estaAutenticado(): boolean {
+    return this.service.estaAutenticado();
   }
 
-  logout() {
-    localStorage.removeItem('tokenUsuarioAutenticado');
+  public sair() {
+    this.service.sair();
     this.router.navigate(['/login']);
   }
 
