f79036bdd6fe7255be2c08f3e3baf99946494808
Maria Leitão <malufln.99@gmail.com>
2024-11-26 11:10:52 -0300

    fix: remove imports

--- src/app/usuario/usuario-detalhe/usuario-detalhe.component.ts
+++ src/app/usuario/usuario-detalhe/usuario-detalhe.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-usuario-detalhe',
-  imports: [],
   templateUrl: './usuario-detalhe.component.html',
   styleUrl: './usuario-detalhe.component.scss'
 })
