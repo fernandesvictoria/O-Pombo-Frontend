fde39022720ff7fb7016568322a3a0ab7ac2446e
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2024-11-26 14:10:21 -0300

    Merge branch 'master' of https://github.com/fernandesvictoria/O-Pombo-Frontend

--- src/app/denuncia/denuncia.module.ts
+++ src/app/denuncia/denuncia.module.ts
@@ -5,12 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { DenunciaDetalheComponent } from './denuncia-detalhe/denuncia-detalhe.component';
 import { DenunciaListagemComponent } from './denuncia-listagem/denuncia-listagem.component';
 import { DenunciaRoutingModule } from './denuncia-routing.module';
+import { MenuModule } from '../menu/menu.module';
 
 @NgModule({
   declarations: [DenunciaDetalheComponent, DenunciaListagemComponent],
-  imports: [
-    CommonModule,
-    DenunciaRoutingModule, HttpClientModule
-  ]
+  imports: [CommonModule, DenunciaRoutingModule, HttpClientModule, MenuModule],
 })
-export class DenunciaModule { }
+export class DenunciaModule {}
