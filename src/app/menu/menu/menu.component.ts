c74ef199de106aed321c184776cf4e5880b6efb9
Maria Leitão <malufln.99@gmail.com>
2024-11-26 12:00:23 -0300

    feat: logout

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -1,39 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { jwtDecode } from 'jwt-decode';
-import { Usuario } from '../../shared/model/usuario';
+import { LoginService } from '../../shared/service/login.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
 })
 export class MenuComponent implements OnInit {
+  constructor(
+    private service: LoginService,
+    private router: Router,
+  ) { }
 
-  public UsuarioAutenticado: Usuario | undefined;
-  public isAdmin: boolean = false;
+  ngOnInit(): void { }
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-    let token = localStorage.getItem('tokenUsuarioAutenticado');
-
-    if (token) {
-      // Converte a String para o formato JSON
-      let tokenJSON: any = jwtDecode(token);
-      this.isAdmin = tokenJSON?.roles == 'ADMINISTRADOR';
-
-      if (this.isAdmin) {
-        this.router.navigate(['/pruus']);
-      }
-    } else {
-      this.router.navigate(['/login']);
-    }
-  }
-
-  logout() {
-    localStorage.removeItem('tokenUsuarioAutenticado');
+  public sair() {
+    this.service.sair();
     this.router.navigate(['/login']);
   }
+
 }
 
 
